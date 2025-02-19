c9c8e6aa7c5bf1f1268614cdfeedc92465ced5c7
Sdftlbzd <95076573+Sdftlbzd@users.noreply.github.com>
2025-02-20 01:54:02 +0400

    edit education

--- src/Core/api/Education/education.route.ts
+++ src/Core/api/Education/education.route.ts
@@ -7,5 +7,5 @@ export const educationRoutes = Router();
 const controller = EducationController();
 
 educationRoutes.post("/create", useAuth, uploads.single("image"), controller.create);
+educationRoutes.put("/update/:id", useAuth,uploads.single("image"), controller.editEducation);
 educationRoutes.delete("/delete", useAuth, controller.deleteEducation);
-
