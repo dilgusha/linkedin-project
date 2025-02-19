d8edc0607635718d55a935b71bb1641983dbed75
murad <kerimovmurad569@gmail.com>
2025-02-19 21:41:29 +0400

    category create

--- src/Core/api/Category/category.controller.ts
+++ src/Core/api/Category/category.controller.ts
@@ -1,32 +1,59 @@
 import { NextFunction, Request, Response } from "express";
 import { Category } from "../../../DAL/models/Category.model";
 
-const deletee = async (req:Request, res:Response, next:NextFunction) => {
-    try {
-        const {id} = req.params
+const createCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, description } = req.body;
 
-        const category = await Category.findOne({
-            where: {id: +id}
-        })
-        
-        if (!category) {
-            res.status(404).json({message: "Category not found"});
-            return;
-        }
-        
-        await category.remove();
-        
-        res.status(200).json({message: "Category deleted successfully"});
-        
-    } catch (error) {
-        
-        res.status(500).json({
-            message: "Internal Server Error",
-            error
-        });
+    if (!name || !description) {
+      res.status(400).json({ message: "Name and description are required" });
+      return;
     }
-}
+
+    const newCategory = new Category();
+    newCategory.name = name;
+    newCategory.description = description;
+
+    await newCategory.save();
+
+    res.status(201).json({
+      message: "Category created successfully",
+      category: newCategory,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
+
+const deletee = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findOne({
+      where: { id: +id },
+    });
+
+    if (!category) {
+      res.status(404).json({ message: "Category not found" });
+      return;
+    }
+
+    await category.remove();
+
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
 
 export const CategoryController = () => ({
-deletee
+  deletee,
+  createCategory,
 });
--- src/Core/api/Category/category.route.ts
+++ src/Core/api/Category/category.route.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { CategoryController } from "./category.controller";
+import { roleCheck, useAuth } from "../../middlewares/auth.middleware";
+import { ERoleType } from "../../app/enums";
 
 export const categoryRoutes = Router();
 const controller = CategoryController();
 
-categoryRoutes.delete("/delete/:id", controller.deletee)
+categoryRoutes.delete("/delete/:id", controller.deletee);
+categoryRoutes.post(
+  "/create",
+  useAuth,
+  roleCheck([ERoleType.ADMIN]),
+  controller.createCategory
+);
